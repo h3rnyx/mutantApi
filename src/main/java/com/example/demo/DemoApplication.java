11c3acc98b20944fdf2bc60810e49e4a6a87b1b2
h3rnyx <hernan_ihf@hotmail.com>
2017-12-23 14:58:44 -0300

    db conection test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @SpringBootApplication
 @RestController
@@ -13,6 +19,35 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	public String welcome() {
-		return "MutantAPI v1.0";
+
+		final String selectSql = "SELECT dna, isMutant FROM dna_mutantes;";
+
+		String url, result = "Welcome!";
+		if (System.getProperty("com.google.appengine.runtime.version").startsWith("Google App Engine/")) {
+			url = System.getProperty("ae-cloudsql.cloudsql-database-url");
+			try {
+				Class.forName("com.mysql.jdbc.GoogleDriver");
+			} catch (ClassNotFoundException e) {
+				System.out.println("Error loading Google JDBC Driver \t| " + e);
+			}
+		} else {
+			url = System.getProperty("ae-cloudsql.local-database-url");
+		}
+		System.out.println("connecting to: " + url);
+		try {
+			Connection conn = DriverManager.getConnection(url);
+			try (ResultSet rs = conn.prepareStatement(selectSql).executeQuery()) {
+				while (rs.next()) {
+					String dna = rs.getString("dna");
+					boolean isMutant = rs.getBoolean("isMutant");
+					result = "DNA: " + dna + " IsMutant: " + isMutant + "\n";
+					System.out.print(result);
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+
+		return result;
 	}
 }
